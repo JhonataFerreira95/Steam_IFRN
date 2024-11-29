4cce36bde27d353c0b287e9c7f4b3deb0f3e39a6
ericksouza95 <erickagente7@gmail.com>
2024-11-29 15:40:42 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent], // Importe os componentes standalone aqui
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'IFRN_projeto';
+  title = 'steam-api-project';
 }
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,12 @@
+import { Component, isStandalone } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.css'
+})
+export class BodyComponent {
+
+}
