d16ac6c76a169cb05314f56330f4f67705429836
ericksouza95 <erickagente7@gmail.com>
2024-11-29 16:49:58 -0300

    update

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+;
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [], // Importe o BodyComponent aqui
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent{
+
+}
+  
