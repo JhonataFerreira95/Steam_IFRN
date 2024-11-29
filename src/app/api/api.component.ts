1b0d54eedb73614d3a0ec4a6b3d133494595d452
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-11-29 17:23:21 -0300

    att api

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-api',
-  standalone: true,
-  imports: [],
   templateUrl: './api.component.html',
-  styleUrl: './api.component.css'
+  styleUrls: ['./api.component.css']
 })
-export class ApiComponent {
+export class ApiComponent implements OnInit {
 
+  perfil: any;
+  steamId: string = 'https://steamcommunity.com/profiles/76561199156125218'; // 
+  apiKey: string = 'SUA_API_KEY_AQUI'; //
+  baseUrl: string = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.buscarPerfil();
+  }
+
+  buscarPerfil(): void {
+    const url = `${this.baseUrl}?key=${this.apiKey}&steamids=${this.steamId}`;
+    this.http.get<any>(url).subscribe(
+      (resposta) => {
+        this.perfil = resposta.response.players[0];
+        console.log(this.perfil);
+      },
+      (erro) => {
+        console.error('Erro ao buscar perfil', erro);
+      }
+    );
+  }
 }
