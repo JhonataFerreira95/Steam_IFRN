541f8cb8cf47bfc12f0c2712cb42c008f82458fe
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-11-29 16:13:47 -0300

    att do standalone

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-body',
+  standalone: true,
   imports: [],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
