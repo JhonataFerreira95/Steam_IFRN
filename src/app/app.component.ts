e58ab91ac193f6bdfa032d8b615212e79509060b
ericksouza95 <erickagente7@gmail.com>
2024-11-29 17:23:39 -0300

    ohmds belo update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { FormsComponent } from './forms/forms.component';
+import { FormsComponent } from './forms/forms.component'; 
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, FooterComponent, BodyComponent, FormsComponent], // Importe os componentes standalone aqui
+  standalone: true,  // Marque o componente como standalone
+  imports: [FormsComponent,HeaderComponent, FooterComponent],  // Adicione outros componentes aqui se necessário
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'steam-api-project';
 }
+export class AppModule { }
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';  // Importando o FormsModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forms',
-  standalone: true,
-  imports: [],
+  standalone: true,   // Marca o componente como standalone
+  imports: [FormsModule, CommonModule],  // Adiciona o FormsModule diretamente aqui
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  styleUrls: ['./forms.component.css']
 })
 export class FormsComponent {
+  userName: string = '';
+  email: string = '';
+  submitted: boolean = false;
 
+  submitForm() {
+    this.submitted = true;
+    console.log('Form Data:', {
+      userName: this.userName,
+      email: this.email
+    });
+  }
 }
