dc654b9c02b597a40e1c74d2f9071e06cee37263
ericksouza95 <erickagente7@gmail.com>
2024-12-05 17:57:19 -0300

    float checker update

--- src/app/float/float.component.ts
+++ src/app/float/float.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-float',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './float.component.html',
   styleUrl: './float.component.css'
 })
 export class FloatComponent {
-
-}
+  inputValue: number = 0; 
+    onSubmit() {
+    console.log('Valor submetido:', this.inputValue);
+  }
+}  
