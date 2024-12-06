cc5e1b71bddc6f3642a356a7449887bac53056e2
ericksouza95 <erickagente7@gmail.com>
2024-12-06 18:47:59 -0300

    ajustes pontuais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,11 @@ import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { FormsComponent } from './forms/forms.component'; 
 import { FloatComponent } from './float/float.component';
-
+import { ApiComponent } from './api/api.component';
 @Component({
   selector: 'app-root',
   standalone: true,  // Marque o componente como standalone
-  imports: [FormsComponent,HeaderComponent, FooterComponent, FloatComponent],  // Adicione outros componentes aqui se necessário
+  imports: [FormsComponent,HeaderComponent, FooterComponent, FloatComponent, ApiComponent],  // Adicione outros componentes aqui se necessário
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
