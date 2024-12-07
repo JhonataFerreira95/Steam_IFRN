f69cb2d8599a3b3a932de4f67c571ee25e327bf7
ericksouza95 <erickagente7@gmail.com>
2024-12-07 17:34:39 -0300

    finalmente o problema de rotas foi kibado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-//import { RouterModule } from '@angular/router';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { FloatComponent } from '../float/float.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsComponent } from '../forms/forms.component';
+
+const routes: Routes = [
+  { path: 'float', component: FloatComponent },
+  { path:  'forms',component: FormsComponent}
+  
+
+
+];
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BsDropdownModule,
+    RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
