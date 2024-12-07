ba6f481430935122a2d9f1740ebfecc516df7a1d
ericksouza95 <erickagente7@gmail.com>
2024-12-07 11:19:22 -0300

    novo float checker

--- src/app/float/float.component.ts
+++ src/app/float/float.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+  
+
+
 @Component({
   selector: 'app-fin',
   standalone: true,
@@ -10,8 +13,29 @@ import { CommonModule } from '@angular/common';
   styleUrl: './float.component.css'
 })
 export class FloatComponent {
-  inputValue: number = 0; 
-    onSubmit() {
-    console.log('Valor submetido:', this.inputValue);
+  floatValue: number = 0; // Inicializado com 0 para evitar 'null'
+  quality: string = ''; // Qualidade da arma
+
+  checkQuality(): void {
+    if (this.floatValue >= 0 && this.floatValue <= 1) {
+      if (this.floatValue <= 0.07) {
+        this.quality = 'Nova de Fábrica (Factory New)';
+      } else if (this.floatValue <= 0.15) {
+        this.quality = 'Pouco Usada (Minimal Wear)';
+      } else if (this.floatValue <= 0.38) {
+        this.quality = 'Testada em Campo (Field-Tested)';
+      } else if (this.floatValue <= 0.45) {
+        this.quality = 'Bem desgastada (Well-Worn)';
+      } else {
+        this.quality = 'Veterana de Guerra (Battle-Scarred)';
+      }
+    } else {
+      this.quality = 'Valor inválido! O float deve estar entre 0 e 1.';
+    }
+
+    // Abrindo o modal
+    const modalElement = document.getElementById('qualityModal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
   }
-}  
+}
