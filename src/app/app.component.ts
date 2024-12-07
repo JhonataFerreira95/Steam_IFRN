205fa31d389015cbe05d5ba311b3e887a2b0ffbd
ericksouza95 <erickagente7@gmail.com>
2024-12-07 10:38:26 -0300

    ajustes no componente float

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
+///import { BodyComponent } from './body/body.component';
 import { FormsComponent } from './forms/forms.component'; 
 import { FloatComponent } from './float/float.component';
 import { ApiComponent } from './api/api.component';
 import { CommonModule } from '@angular/common';
-
+import { UserCardComponent } from './user-card/user-card.component';
 @Component({
   selector: 'app-root',
   standalone: true,  // Marque o componente como standalone
-  imports: [FormsComponent,HeaderComponent, FooterComponent, FloatComponent, CommonModule, ApiComponent],  // Adicione outros componentes aqui se necessário
+  imports: [FormsComponent,HeaderComponent, FooterComponent, FloatComponent, CommonModule, ApiComponent, UserCardComponent],  // Adicione outros componentes aqui se necessário
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
