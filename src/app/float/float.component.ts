479ef726fd93922a7577e125cfde1fe6cbbebbb3
Jhonata Ferreira <87663377+JhonataFerreira95@users.noreply.github.com>
2025-02-04 11:00:45 -0300

    feito a  classe de cor para a qualidade

--- src/app/float/float.component.ts
+++ src/app/float/float.component.ts
@@ -2,20 +2,17 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-  
-
-
 @Component({
   selector: 'app-fin',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './float.component.html',
   styleUrls: ['./float.component.scss']
 })
-
 export class FloatComponent {
   floatValue: number = 0; // Inicializado com 0 para evitar 'null'
-  quality: string = 'sdfds'; // Qualidade da arma
+  quality: string = '';   // Qualidade da arma
+  qualityClass: string = ''; // Classe de cor para a qualidade
 
   constructor(private cdRef: ChangeDetectorRef) {}
 
@@ -23,20 +20,28 @@ export class FloatComponent {
     if (this.floatValue >= 0 && this.floatValue <= 1) {
       if (this.floatValue <= 0.07) {
         this.quality = 'Nova de Fábrica (Factory New)';
+        this.qualityClass = 'text-success'; // Verde
       } else if (this.floatValue <= 0.15) {
         this.quality = 'Pouco Usada (Minimal Wear)';
+        this.qualityClass = 'text-warning'; // Amarelo
       } else if (this.floatValue <= 0.38) {
         this.quality = 'Testada em Campo (Field-Tested)';
+        this.qualityClass = 'text-warning'; // Amarelo
       } else if (this.floatValue <= 0.45) {
         this.quality = 'Bem desgastada (Well-Worn)';
+        this.qualityClass = 'text-danger'; // Vermelho
       } else {
         this.quality = 'Veterana de Guerra (Battle-Scarred)';
+        this.qualityClass = 'text-danger'; // Vermelho
       }
     } else {
       this.quality = 'Valor inválido! O float deve estar entre 0 e 1.';
+      this.qualityClass = 'text-danger'; // Vermelho para erro
     }
-    // forçar detecção de mudança
+
+    // Forçar detecção de mudança
     this.cdRef.detectChanges();
+
     // Abrindo o modal
     const modalElement = document.getElementById('qualityModal');
     const modal = new bootstrap.Modal(modalElement);
